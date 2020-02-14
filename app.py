d8559e4e421bd5047813847e823bb0cde14625a6
Boglarka Barabas <taikatta@gmail.com>
2020-02-14 12:19:59 +0000

    Flask and MongoDB connect

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+
+app.config["MONGO_DBNAME"] = 'konyvkucko'
+app.config["MONGO_URI"] = 'mongodb+srv://root:m0ng0database@myfirstcluster-ptc6u.mongodb.net/konyvkucko?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
     return 'Konyvkucko'
 
-if __name__ == '__main__':
+@app.route('/getbooks')
+def getbooks():
+    return render_template("books.html", books=mongo.db.books.find())
+
+
+if __name__ =="__main__":
     app.run(host=os.environ.get('IP', default='127.0.0.1'), 
         port=int(os.environ.get('PORT', default=5000)), 
-        debug=True)
+        debug=False)
