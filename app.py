ef8aaf441e619664ee7a6e3c7a90db199c7fe9dc
Boglarka Barabas <taikatta@gmail.com>
2020-02-24 11:24:46 +0000

    Delete book added

--- app.py
+++ app.py
@@ -73,6 +73,10 @@ def update_book(book_id):
 
     return redirect(url_for('allbooks'))
 
+@app.route('/delete_book/<book_id>')
+def delete_book(book_id):
+    mongo.db.books.remove({'_id': ObjectId(book_id)})
+    return redirect(url_for('allbooks'))
 
 
 if __name__ == '__main__':
