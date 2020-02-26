35ef9771931b73231c6615350e2b3a86d4d9b4d7
Boglarka Barabas <taikatta@gmail.com>
2020-02-26 21:41:08 +0000

    Datepicker added

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,17 @@ $(document).ready(function() {
     $('select').material_select();
     $('.button-collapse').sideNav();
 });
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+});
+document.getElementById("matfix").addEventListener("click", function (e) {
+    e.stopPropagation();
+});
 
 <!-- I am using Materialize for card reveal, but wanted to change it to reveal the summary by hover. I got the solution from stack overflow: https://bit.ly/39S4X6f -->
 $(function () {
