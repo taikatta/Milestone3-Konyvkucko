cbd0e1607f2e37c068d93a6c0045bb9b4a70ab4c
Boglarka Barabas <taikatta@gmail.com>
2020-02-28 14:17:45 +0000

    SECRET_KEY added to app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 
-"""
+
 # Secret Key value generated via secrets python module.
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-"""
+
 
 
 mongo = PyMongo(app)
