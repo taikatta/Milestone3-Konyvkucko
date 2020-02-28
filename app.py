0f31ba97f0b8cb642a7b2addf083a8bcef450d03
Boglarka Barabas <taikatta@gmail.com>
2020-02-28 14:49:08 +0000

    env.py: os.environ.setdefault updated to os.environ

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
+"""
 
 # Secret Key value generated via secrets python module.
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-
+"""
 
 mongo = PyMongo(app)
 
