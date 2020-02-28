6b786c518e31303b0f883d33f2899b8322872919
Boglarka Barabas <taikatta@gmail.com>
2020-02-28 13:28:09 +0000

    __pycache__/ added to .gitignore

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import os
+from os import path
 from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+if path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
-
-
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 
-
+"""
 # Secret Key value generated via secrets python module.
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-
+"""
 
 
 mongo = PyMongo(app)
